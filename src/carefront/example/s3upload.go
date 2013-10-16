802adeeafb3acc6ec5a9cbf69fd62db61bc90ca5
Kunal Jham <kunaljham@unknownc8e0eb1825a1.att.net>
2013-10-16 11:48:57 -0700

    - testing out how to list keys in a bucket

--- src/carefront/example/s3upload.go
+++ src/carefront/example/s3upload.go
@@ -4,7 +4,6 @@ import (
 	"launchpad.net/goamz/aws"
 	"launchpad.net/goamz/s3"
 	"fmt"
-	"time"
 )
 
 func main() {
@@ -16,15 +15,10 @@ func main() {
 	s3Access := s3.New(auth, aws.USWest) 
 	s3Bucket := s3Access.Bucket("carefront-cases")
 
-	additionalHeaders := map[string][]string {
-		"x-amz-server-side-encryption" : {"AES256"},	
-	}
-
-	err = s3Bucket.Put("testing/testingAnotherFile.txt", make([]byte, 1000), "binary/octet-stream", s3.BucketOwnerFull, additionalHeaders)
+	listBucketResult, err := s3Bucket.List("1234/", "/", "", 100)
 	if err != nil {
-		panic(err.Error())
+		panic(err.Error())	
 	}
 	
-	signedUrl := s3Bucket.SignedURL("testing/testing1234", time.Now().Add(10*time.Second))
-	fmt.Println(signedUrl)
+	fmt.Println("%q", listBucketResult.Contents)
 }
