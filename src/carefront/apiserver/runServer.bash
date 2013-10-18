32dec4643467d0aef9424638abd486ca6b37c75b
Kunal Jham <kunaljham@unknownc8e0eb1825a1.att.net>
2013-10-18 11:01:26 -0700

    - removing CC environment variable

--- src/carefront/apiserver/runServer.bash
+++ src/carefront/apiserver/runServer.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CC=clang ./apiserver -cert_key=cert.pem -private_key=key.pem -case_bucket=carefront-cases -aws_secret_key="eGc+Y28/t7q8LcgnYkSZyi5H8D4tzpSeFSt/158S" -aws_access_key=AKIAJVJQW6IJT7ZOQMWA -db_user=ejabberd -db_name=carefront_db -db_password=ejabberd -db_host=ejabberd-db-dev.c83wlsbcftxz.us-west-1.rds.amazonaws.com
+./apiserver -cert_key=cert.pem -private_key=key.pem -case_bucket=carefront-cases -aws_secret_key="eGc+Y28/t7q8LcgnYkSZyi5H8D4tzpSeFSt/158S" -aws_access_key=AKIAJVJQW6IJT7ZOQMWA -db_user=ejabberd -db_name=carefront_db -db_password=ejabberd -db_host=ejabberd-db-dev.c83wlsbcftxz.us-west-1.rds.amazonaws.com
