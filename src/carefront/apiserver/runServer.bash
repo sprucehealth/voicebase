5b334083f982780c9f6af2d2a03792b3cc3d9fb9
Kunal Jham <kunaljham@unknownc8e0eb1825a1.att.net>
2013-10-18 11:00:24 -0700

    - removing the goBuild script since its no longer needed - updating the runServer script to use command line arguments versus provide environment variables

--- src/carefront/apiserver/runServer.bash
+++ src/carefront/apiserver/runServer.bash
@@ -1,13 +1,2 @@
 #!/usr/bin/env bash
-CERT_KEY=cert.pem \
-PRIVATE_KEY=key.pem \
-CASE_BUCKET=carefront-cases \
-GOPATH=/Users/kunaljham/Dropbox/personal/workspace/backend/medellin \
-AWS_SECRET_ACCESS_KEY="eGc+Y28/t7q8LcgnYkSZyi5H8D4tzpSeFSt/158S" \
-AWS_ACCESS_KEY_ID=AKIAJVJQW6IJT7ZOQMWA \
-DB_USER=ejabberd \
-DB_PASSWORD=ejabberd \
-DB_HOST=ejabberd-db-dev.c83wlsbcftxz.us-west-1.rds.amazonaws.com \
-DB_NAME=carefront_db \
-CC=clang \
-./apiserver
+CC=clang ./apiserver -cert_key=cert.pem -private_key=key.pem -case_bucket=carefront-cases -aws_secret_key="eGc+Y28/t7q8LcgnYkSZyi5H8D4tzpSeFSt/158S" -aws_access_key=AKIAJVJQW6IJT7ZOQMWA -db_user=ejabberd -db_name=carefront_db -db_password=ejabberd -db_host=ejabberd-db-dev.c83wlsbcftxz.us-west-1.rds.amazonaws.com
