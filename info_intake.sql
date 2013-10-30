0f242f27a014d5499de081bcdee3a9741fc704b1
Kunal Jham <kunaljham@kunals-mbp.att.net>
2013-10-30 06:30:21 -0700

    - adding answered_date to the patient_info_intake table so as to have a   date associated with each information intake - adding a layout_version table to keep track of which layout version is   used for what piece of information - changed the usage of int(11) to int unsigned so as to give us about 4B   entries into the table.

--- info_intake.sql
+++ info_intake.sql
@@ -1,7 +1,7 @@
 use info_intake_db;
 
 CREATE TABLE IF NOT EXISTS app_text (
-	id int(11) NOT NULL AUTO_INCREMENT,
+	id unsigned int NOT NULL AUTO_INCREMENT,
 	comment varchar(600),
 	app_text_tag varchar(250) NOT NULL,
 	PRIMARY KEY (id),
@@ -9,29 +9,26 @@ CREATE TABLE IF NOT EXISTS app_text (
 ) CHARACTER SET utf8;
 
 CREATE TABLE IF NOT EXISTS localized_text (
-	id int(11) NOT NULL AUTO_INCREMENT,
+	id unsigned int NOT NULL AUTO_INCREMENT,
 	language varchar(5) NOT NULL,
 	ltext varchar(600) NOT NULL,
-	app_text_id int(11) NOT NULL,
+	app_text_id int unsigned NOT NULL,
 	FOREIGN KEY (app_text_id) REFERENCES app_text(id) ON DELETE CASCADE,
 	PRIMARY KEY (id)
 ) CHARACTER SET utf8;
 
 CREATE TABLE IF NOT EXISTS question_type (
-	id int(11) NOT NULL AUTO_INCREMENT,
+	id unsigned int NOT NULL AUTO_INCREMENT,
 	qType varchar(250),
 	PRIMARY KEY (id)
 ) CHARACTER SET utf8;
 
-// unique constraints
-// date associated with each answer
-// there should be a way to specify previously selected answers.
 CREATE TABLE IF NOT EXISTS question (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	qType_id int(11) NOT NULL,
-	qtext_app_text_id int(11) NOT NULL,
-	subtext_app_text_id int(11) NOT NULL,
-	section_id int(11) NOT NULL,
+	id unsigned int NOT NULL AUTO_INCREMENT,
+	qType_id int unsigned NOT NULL,
+	qtext_app_text_id int unsigned NOT NULL,
+	subtext_app_text_id int unsigned NOT NULL,
+	section_id int unsigned NOT NULL,
 	question_tag varchar(250) NOT NULL,
 	FOREIGN KEY (qType_id) REFERENCES question_type(id),
 	FOREIGN KEY (subtext_app_text_id) REFERENCES app_text(id),
@@ -42,16 +39,16 @@ CREATE TABLE IF NOT EXISTS question (
 ) CHARACTER SET utf8;
 
 CREATE TABLE IF NOT EXISTS outcome_type (
-	id int(11) NOT NULL AUTO_INCREMENT,
+	id int unsigned NOT NULL AUTO_INCREMENT,
 	otype varchar(250),
 	PRIMARY KEY (id)
 ) CHARACTER SET utf8;
 
 CREATE TABLE IF NOT EXISTS potential_outcome (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	question_id int(11) NOT NULL,
-	outcome_localized_text int(11) NOT NULL,
-	otype_id int(11),
+	id int unsigned NOT NULL AUTO_INCREMENT,
+	question_id int unsigned NOT NULL,
+	outcome_localized_text int unsigned NOT NULL,
+	otype_id int unsigned,
 	potential_outcome_tag varchar(250) NOT NULL,
 	FOREIGN KEY (otype_id) REFERENCES outcome_type(id),
 	FOREIGN KEY (question_id) REFERENCES question(id),
@@ -61,10 +58,10 @@ CREATE TABLE IF NOT EXISTS potential_outcome (
 ) CHARACTER SET utf8;
 
 CREATE TABLE IF NOT EXISTS section (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	section_title_app_text_id int(11) NOT NULL,
+	id int unsigned NOT NULL AUTO_INCREMENT,
+	section_title_app_text_id int unsigned NOT NULL,
 	comment varchar(600) NOT NULL,
-	treatment_id int(11) NOT NULL,
+	treatment_id int unsigned NOT NULL,
 	section_tag varchar(250) NOT NULL,
 	FOREIGN KEY (section_title_app_text_id) REFERENCES app_text(id),
 	PRIMARY KEY (id),	
@@ -72,19 +69,32 @@ CREATE TABLE IF NOT EXISTS section (
 ) CHARACTER SET utf8;
 
 CREATE TABLE IF NOT EXISTS treatment (
-	id int(11) NOT NULL AUTO_INCREMENT,
+	id int unsigned NOT NULL AUTO_INCREMENT,
 	comment varchar(600) NOT NULL,
 	PRIMARY KEY (id)
 ) CHARACTER SET UTF8;
 
 CREATE TABLE IF NOT EXISTS patient_info_intake (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	treatment_id int(11) NOT NULL,
-	question_id int(11) NOT NULL,
-	potential_outcome_id int(11) NOT NULL,
-	outcome_text 
+	id int unsigned NOT NULL AUTO_INCREMENT,
+	treatment_id int unsigned NOT NULL,
+	question_id int unsigned NOT NULL,
+	potential_outcome_id int unsigned NOT NULL,
+	outcome_text varchar(600),
+	layout_version_id int unsigned NOT NULL,
+	answered_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id),
 	FOREIGN KEY (question_id) REFERENCES question(id),
+	FOREIGN KEY (layout_version_id) REFERENCES layout_version(id),
 ) CHARACTER SET UTF8;
+
+CREATE TABLE IF NOT EXISTS layout_version (
+	id int unsigned NOT NULL AUTO_INCREMENT,
+	url varchar(250) NOT NULL,
+	treatment_id int unsigned NOT NULL,
+	comment varchar(600),
+	status varchar(250) NOT NULL, 
+	FOREIGN KEY (treatment_id) REFERENCES treatment(id),
+	PRIMARY KEY (id)
+) CHARACTER SET utf8;
 
 
