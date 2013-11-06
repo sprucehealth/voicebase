df41f001aacfcc19ef181f12831d97e300b618a3
Kunal Jham <kunal@carefront.net>
2013-11-06 13:28:41 -0800

    - adding comment about how queries can be made better

--- src/carefront/layout_transformer/layout_generator.go
+++ src/carefront/layout_transformer/layout_generator.go
@@ -9,6 +9,9 @@ type ClientLayoutProcessor struct {
 }
 
 func (c *ClientLayoutProcessor) TransformIntakeIntoClientLayout(treatment *Treatment) error {
+	// TODO currently, calling the FillDataBaseInfo results in each section, questio, potential outcome and tip
+	// making indepedent roundtrips to the database, as opposed to batch querying the database which would save time
+	// and improve performance
 	err := treatment.FillInDatabaseInfo(c.DataApi)
 	if err != nil {
 		return err
