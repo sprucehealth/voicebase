46c6f3c6afdcb4e501c3f01baa10ddbae840809c
Kunal Jham <kunal@carefront.net>
2013-11-23 12:32:48 -0800

    - Ignoring fields that will not be sent to client

--- src/carefront/common/models.go
+++ src/carefront/common/models.go
@@ -2,17 +2,19 @@ package common
 
 type PatientAnswer struct {
 	PatientAnswerId   int64            `json:"patient_answer_id,string"`
-	QuestionId        int64            `json:"question_id,omitempty,string"`
-	PatientId         int64            `json:"patient_id",omitempty,string"`
-	PatientVisitId    int64            `json:"patient_visit_id,omitempty,string"`
-	ParentQuestionId  int64            `json:"parent_question_id,string"`
-	ParentAnswerId    int64            `json:"parent_answer_id,string"`
+	QuestionId        int64            `json:"-"`
+	PatientId         int64            `json:"-"`
+	PatientVisitId    int64            `json:"-"`
+	ParentQuestionId  int64            `json:"-"`
+	ParentAnswerId    int64            `json:"-"`
 	PotentialAnswerId int64            `json:"potential_answer_id,string"`
-	LayoutVersionId   int64            `json:"layout_version_id,string,omitempty"`
+	PotentialAnswer   string           `json:"potential_answer,omitempty"`
+	AnswerSummary     string           `json:"answer_summary,omitempty"`
+	LayoutVersionId   int64            `json:"-"`
 	SubAnswers        []*PatientAnswer `json:"answers,omitempty"`
 	AnswerText        string           `json:"answer_text,omitempty"`
 	ObjectUrl         string           `json:"object_url,omitempty"`
-	StorageBucket     string           `json:"storage_bucket,omitempty"`
-	StorageKey        string           `json:"storage_key,omitempty"`
-	StorageRegion     string           `json:"storage_region,omitempty"`
+	StorageBucket     string           `json:"-"`
+	StorageKey        string           `json:"-"`
+	StorageRegion     string           `json:"-"`
 }
