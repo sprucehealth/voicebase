32c50796876c2cef29351952f67edf2269d5d225
Kunal Jham <kunal@carefront.net>
2013-11-27 19:59:20 -0800

    - don't need the script anymore to make schema to load from current   snapsho

--- src/carefront/mysql/make_latest_snapshot_current.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-latestSnapshotNumber=`ls -r snapshot-*.sql | cut -d- -f 2  | cut -d. -f1 | sort -nr | head -1`
-echo "create database database_$TRAVIS_BUILD_ID; use database_$TRAVIS_BUILD_ID;"  | cat - snapshot-$latestSnapshotNumber.sql > current.sql
-echo "Going to load latest schame into database_$TRAVIS_BUILD_ID"
