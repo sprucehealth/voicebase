dbe4abe1e7472ac0221a0468b189f45f3a213416
Kunal Jham <kunal@carefront.net>
2013-11-27 19:20:13 -0800

    - testing the usage of an RDS instance for testing purposes

--- src/carefront/mysql/make_latest_snapshot_current.sh
+++ src/carefront/mysql/make_latest_snapshot_current.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
 latestSnapshotNumber=`ls -r snapshot-*.sql | cut -d- -f 2  | cut -d. -f1 | sort -nr | head -1`
-echo "create database carefront_test; use carefront_test;"  | cat - snapshot-$latestSnapshotNumber.sql > current.sql
+echo "create database database_$TRAVIS_BUILD_ID; use database_$TRAVIS_BUILD_ID;"  | cat - snapshot-$latestSnapshotNumber.sql > current.sql
+echo "Going to load latest schame into database_$TRAVIS_BUILD_ID"
