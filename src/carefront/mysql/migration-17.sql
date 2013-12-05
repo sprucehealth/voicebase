688e89926e6930988768b68a766d7dfabfe3c7b4
Kunal Jham <kunal@carefront.net>
2013-12-05 14:52:23 -0800

    - adding more additional fields for the autocomplete questions

--- src/carefront/mysql/migration-17.sql
+++ src/carefront/mysql/migration-17.sql
@@ -1,14 +1,29 @@
+use carefront_db;
+
 -- Adding fields for each of the autocomplete questions
 start transaction;
 
 set @en_language_id = (select id from languages_supported where language="en");
 
-insert into app_text (app_text_tag, comment) values ('txt_add_treatment', 'txt for prompting user to add treatment');
-insert into localized_text (language_id, app_text_id, ltext) values (@en_language_id, (select id from app_text where app_text_tag = 'txt_add_treatment'), 'Add a Treatment');
-insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_acne_prev_treatment_list'), 'add_text', (select id from app_text where app_text_tag = 'txt_add_treatment'));
+insert into app_text (app_text_tag, comment) values ('txt_add_medication', 'txt for prompting user to add medication');
+insert into localized_text (language_id, app_text_id, ltext) values (@en_language_id, (select id from app_text where app_text_tag = 'txt_add_medication'), 'Add a Medication');
+insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_allergic_medication_entry'), 'add_text', (select id from app_text where app_text_tag = 'txt_add_medication'));
 
-insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_acne_prev_treatment_list'), 'placeholder_text', (select id from app_text where app_text_tag = 'txt_type_add_treatment'));
+update app_text set app_text_tag='txt_type_add_medication' where app_text_tag='txt_add_medicataion';
+insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_allergic_medication_entry'), 'placeholder_text', (select id from app_text where app_text_tag = 'txt_type_add_medication'));
 
-insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_acne_prev_treatment_list'), 'add_photo_text', (select id from app_text where app_text_tag = 'txt_take_photo_treatment'));
+insert into app_text (app_text_tag, comment) values ('txt_take_photo_medication', 'txt for prompting user to take a photo of the medication');
+insert into localized_text (language_id, app_text_id, ltext) values (@en_language_id, (select id from app_text where app_text_tag = 'txt_take_photo_medication'), 'Or take a photo of the medication');
+insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_allergic_medication_entry'), 'add_photo_text', (select id from app_text where app_text_tag = 'txt_take_photo_medication'));
+
+
+insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_current_medications_entry'), 'add_text', (select id from app_text where app_text_tag = 'txt_add_medication'));
+insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_current_medications_entry'), 'placeholder_text', (select id from app_text where app_text_tag = 'txt_type_add_medication'));
+insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_current_medications_entry'), 'add_photo_text', (select id from app_text where app_text_tag = 'txt_take_photo_medication'));
+
+
+insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_topical_allergies_medication_entry'), 'add_text', (select id from app_text where app_text_tag = 'txt_add_medication'));
+insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_topical_allergies_medication_entry'), 'placeholder_text', (select id from app_text where app_text_tag = 'txt_type_add_medication'));
+insert into question_fields (question_id, question_field, app_text_id) values ((select id from question where question_tag = 'q_topical_allergies_medication_entry'), 'add_photo_text', (select id from app_text where app_text_tag = 'txt_take_photo_medication'));
 
 commit;
