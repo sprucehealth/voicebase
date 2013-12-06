78be252ba2ec5f82bac05efd90c63eccabb2a977
Samuel Stauffer <samuel@carefront.net>
2013-12-05 16:12:45 -0800

    More subtle test colorizer

--- tools/gotestcolorize.py
+++ tools/gotestcolorize.py
@@ -1,35 +1,25 @@
 #!/usr/bin/env python
 
-import os
 import sys
 
-PASS = "\033[0;32m"
-FAIL = "\033[1;31m"
-END = "\033[0m"
+COLOR_BRIGHT_WHITE = "\033[1;97m"
+COLOR_BOLD_RED = "\033[1;31m"
+COLOR_GREEN = "\033[0;32m"
+COLOR_NORMAL = "\033[0m"
+CHECK_MARK = "\xe2\x9c\x93"
 
-def output_run(run, success):
-    for line in run:
-        if success:
-            sys.stdout.write(PASS)
-        elif success == False:
-            sys.stdout.write(FAIL)
-        sys.stdout.write(line+"\n")
-        sys.stdout.write(END)
+status = 0
+for line in sys.stdin:
+    if line.startswith("=== RUN "):
+        sys.stdout.write("\n")
+        sys.stdout.write(COLOR_BRIGHT_WHITE)
+    elif line.startswith("--- PASS:"):
+        sys.stdout.write(COLOR_GREEN)
+    elif line.startswith("--- FAIL:") or line.startswith("FAIL"):
+        sys.stdout.write(COLOR_BOLD_RED)
+        status = 1
+    sys.stdout.write(line)
+    sys.stdout.write(COLOR_NORMAL)
     sys.stdout.flush()
 
-run = []
-success = None
-for line in sys.stdin:
-    line = line.strip()
-    if line.startswith("=== RUN "):
-        if run:
-            output_run(run, success)
-        run = []
-        success = None
-    elif line.startswith("--- PASS:"):
-        success = True
-    elif line.startswith("--- FAIL:"):
-        success = False
-    run.append(line)
-if run:
-    output_run(run, success)
+sys.exit(status)
