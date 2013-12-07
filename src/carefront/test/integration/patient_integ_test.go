d5876720aceca3654f5de92990429ba6d17ce19b
Kunal Jham <kunal@carefront.net>
2013-12-06 18:05:19 -0800

    - adding integration test for case submission

--- src/carefront/test/integration/patient_integ_test.go
+++ src/carefront/test/integration/patient_integ_test.go
@@ -3,12 +3,13 @@ package integration
 import (
 	// "encoding/json"
 	// "fmt"
-	// "io/ioutil"
-	// "net/http"
-	// "net/http/httptest"
-
-	// "carefront/apiservice"
+	"bytes"
+	"carefront/apiservice"
 	_ "github.com/go-sql-driver/mysql"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
@@ -42,5 +43,53 @@ func TestPatientVisitCreation(t *testing.T) {
 	anotherPatientVisitResponse := GetPatientVisitForPatient(signedupPatientResponse.PatientId, testData, t)
 	if anotherPatientVisitResponse.PatientVisitId != patientVisitResponse.PatientVisitId {
 		t.Fatal("The patient visit id for subsequent calls should be the same so long as we have not closed/submitted the case")
+	}
+}
+
+func TestPatientVisitSubmission(t *testing.T) {
+	if err := CheckIfRunningLocally(t); err == CannotRunTestLocally {
+		return
+	}
+	testData := SetupIntegrationTest(t)
+	defer testData.DB.Close()
+
+	signedupPatientResponse := SignupRandomTestPatient(t, testData.DataApi, testData.AuthApi)
+	patientVisitResponse := GetPatientVisitForPatient(signedupPatientResponse.PatientId, testData, t)
+
+	patientVisitHandler := apiservice.NewPatientVisitHandler(testData.DataApi, testData.AuthApi,
+		testData.CloudStorageService, testData.CloudStorageService)
+	patient, err := testData.DataApi.GetPatientFromId(signedupPatientResponse.PatientId)
+	if err != nil {
+		t.Fatal("Unable to get patient information given the patient id: " + err.Error())
+	}
+
+	patientVisitHandler.AccountIdFromAuthToken(patient.AccountId)
+	ts := httptest.NewServer(patientVisitHandler)
+	defer ts.Close()
+	buffer := bytes.NewBufferString("patient_visit_id=")
+	buffer.WriteString(strconv.FormatInt(patientVisitResponse.PatientVisitId, 10))
+	resp, err := http.Post(ts.URL, "application/x-www-form-urlencoded", buffer)
+
+	if err != nil {
+		t.Fatal("Unable to get the patient visit id")
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("Unable to read body of the response for the new patient visit call: " + err.Error())
+	}
+
+	CheckSuccessfulStatusCode(resp, "Unsuccessful call to register new patient visit: "+string(body), t)
+
+	// get the patient visit information to ensure that the case has been submitted
+	patientVisit, err := testData.DataApi.GetPatientVisitFromId(patientVisitResponse.PatientVisitId)
+	if patientVisit.Status != "SUBMITTED" {
+		t.Fatalf("Case status should be submitted after the case was submitted to the doctor, but its not. It is %s instead.", patientVisit.Status)
+	}
+
+	// now, the patient_visit returned should be diffeent than the previous one
+	anotherPatientVisitResponse := GetPatientVisitForPatient(signedupPatientResponse.PatientId, testData, t)
+	if anotherPatientVisitResponse.PatientVisitId == patientVisitResponse.PatientVisitId {
+		t.Fatal("The patient visit id should be different as a new visit should start after the patient has submitted a patient visit")
 	}
 }
