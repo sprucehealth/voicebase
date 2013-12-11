6cdda0a00eb8154c945c826e2367bb6fc75d47ad
Kunal Jham <kunal@carefront.net>
2013-12-11 11:59:34 -0800

    - using slices of bytes instead of strings

--- src/carefront/libs/erx/singlesignon.go
+++ src/carefront/libs/erx/singlesignon.go
@@ -1,12 +1,11 @@
 package erx
 
 import (
+	"bytes"
 	"crypto/sha512"
 	"encoding/base64"
-	"io"
 	"math/rand"
 	"os"
-	"strings"
 	"time"
 )
 
@@ -28,62 +27,63 @@ func GenerateSingleSignOn() SingleSignOn {
 
 	// STEP 1: Create a random phrase 32 characters long in UTF8
 	phrase := generateRandomAlphaNumString(32)
-	singleSignOn.Code = createSingleSignOn(phrase, clinicKey)
-	singleSignOn.UserIdVerify = createSingleSignOnUserIdVerify(phrase, clinicKey, userId)
+	singleSignOn.Code = string(createSingleSignOn(phrase, clinicKey))
+	singleSignOn.UserIdVerify = string(createSingleSignOnUserIdVerify(phrase, clinicKey, userId))
 
 	return singleSignOn
 }
 
-func generateRandomAlphaNumString(n int) string {
+func generateRandomAlphaNumString(n int) []byte {
 	randomBytes := make([]byte, n)
 	for i := 0; i < n; i++ {
 		randomBytes[i] = alphanum[rand.Intn(len(alphanum))]
 	}
-	return string(randomBytes)
+	return randomBytes
 }
 
 // Steps to create the singleSignOnUserIdVerify is spelled out in the
-func createSingleSignOnUserIdVerify(phrase, clinicKey, userId string) string {
+func createSingleSignOnUserIdVerify(phrase []byte, clinicKey, userId string) []byte {
 
 	// STEP 1: first 22 characters from phrase
 	first22Pharse := phrase[:22]
 
 	// STEPS 2-5: Compute the hash of the userId + first 22 characters from phrase + key
 	sha512Hash := sha512.New()
-	io.WriteString(sha512Hash, userId)
-	io.WriteString(sha512Hash, first22Pharse)
-	io.WriteString(sha512Hash, clinicKey)
+	sha512Hash.Write([]byte(userId))
+	sha512Hash.Write(first22Pharse)
+	sha512Hash.Write([]byte(clinicKey))
 	hashedBytes := sha512Hash.Sum(nil)
 
 	// STEP 6: Get a Base64String out of the hash that you created
-	base64string := base64.StdEncoding.EncodeToString(hashedBytes)
+	encodedBytes := make([]byte, base64.StdEncoding.EncodedLen(len(hashedBytes)))
+	base64.StdEncoding.Encode(encodedBytes, hashedBytes)
 
 	// STEP 7: if there are two = signs at the end, then remove them.
-	base64string = removeTwoEqualSignsIfPresent(base64string)
-
-	return base64string
+	base64Encoded := removeTwoEqualSignsIfPresent(encodedBytes)
+	return base64Encoded
 }
 
-func createSingleSignOn(phrase, clinicKey string) string {
+func createSingleSignOn(phrase []byte, clinicKey string) []byte {
 
 	// STEPS 2 - 4: Compute the hash of the phrase concatenated with the key
 	sha512Hash := sha512.New()
-	io.WriteString(sha512Hash, phrase)
-	io.WriteString(sha512Hash, clinicKey)
-	hasedBytes := sha512Hash.Sum(nil)
+	sha512Hash.Write(phrase)
+	sha512Hash.Write([]byte(clinicKey))
+	hashedBytes := sha512Hash.Sum(nil)
 
 	// STEP 5: Get a Base64String out of the hash that you created
-	base64string := base64.StdEncoding.EncodeToString(hasedBytes)
+	encodedBytes := make([]byte, base64.StdEncoding.EncodedLen(len(hashedBytes)))
+	base64.StdEncoding.Encode(encodedBytes, hashedBytes)
 
 	// STEP 6: If there are two = signs at the end, then remove them.
-	base64string = removeTwoEqualSignsIfPresent(base64string)
+	base64Encoded := removeTwoEqualSignsIfPresent(encodedBytes)
 
 	// STEP 7: Prepend the same random phrase from step 2 to your code.
-	return phrase + base64string
+	return append(phrase, base64Encoded...)
 }
 
-func removeTwoEqualSignsIfPresent(str string) string {
-	if strings.HasSuffix(str, "==") {
+func removeTwoEqualSignsIfPresent(str []byte) []byte {
+	if bytes.Compare(str[len(str)-2:len(str)], []byte("==")) == 0 {
 		return str[:len(str)-2]
 	}
 	return str
