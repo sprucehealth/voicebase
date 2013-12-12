2df5e6628e40582cca140083c3084dd339456b44
Kunal Jham <kunal@carefront.net>
2013-12-12 10:14:31 -0800

    - fix test

--- src/carefront/libs/erx/singlesignon_test.go
+++ src/carefront/libs/erx/singlesignon_test.go
@@ -1,11 +1,15 @@
 package erx
 
 import (
+	"os"
 	"testing"
 )
 
 func BenchmarkSingleSignonGeneration(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		generateSingleSignOn()
+		clinicKey := os.Getenv("DOSESPOT_CLINIC_KEY")
+		userId := os.Getenv("DOSESPOT_USER_ID")
+		clinicId := os.Getenv("DOSESPOT_CLINIC_ID")
+		generateSingleSignOn(clinicKey, userId, clinicId)
 	}
 }
