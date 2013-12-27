4fa640896a90832e599dcecebfab8c80cec1ae95
Kunal Jham <kunal@carefront.net>
2013-12-26 23:26:32 -0800

    - add test for care provider elligibility check

--- src/carefront/test/integration/patient_integ_test.go
+++ src/carefront/test/integration/patient_integ_test.go
@@ -1,7 +1,12 @@
 package integration
 
 import (
+	"carefront/apiservice"
+	"carefront/libs/maps"
 	_ "github.com/go-sql-driver/mysql"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -9,6 +14,43 @@ func TestPatientRegistration(t *testing.T) {
 	testData := SetupIntegrationTest(t)
 	defer TearDownIntegrationTest(t, testData)
 	SignupRandomTestPatient(t, testData.DataApi, testData.AuthApi)
+}
+
+func TestPatientCareProvidingEllgibility(t *testing.T) {
+	if err := CheckIfRunningLocally(t); err == CannotRunTestLocally {
+		return
+	}
+	testData := SetupIntegrationTest(t)
+	defer TearDownIntegrationTest(t, testData)
+
+	checkElligibilityHandler := &apiservice.CheckCareProvidingElligibilityHandler{DataApi: testData.DataApi, MapsService: maps.GoogleMapsService(0)}
+	ts := httptest.NewServer(checkElligibilityHandler)
+	resp, err := http.Get(ts.URL + "?zip_code=94115")
+
+	if err != nil {
+		t.Fatal("Unable to successfuly check care providing elligiblity for patient " + err.Error())
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("Unable to successfuly read the body of the response")
+	}
+
+	CheckSuccessfulStatusCode(resp, "Unable to make a successful call to check for care providing elligibility: "+string(body), t)
+
+	resp, err = http.Get(ts.URL + "?zip_code=33180")
+	if err != nil {
+		t.Fatal("Unable to successfuly check care providing elligibility for patient" + err.Error())
+	}
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("Unable to read the response from the body for patient care providing elligibility check: " + err.Error())
+	}
+
+	if resp.StatusCode != http.StatusForbidden {
+		t.Fatalf("Expected the status code to be 403, but got a %d instead", resp.StatusCode)
+	}
 }
 
 func TestPatientVisitCreation(t *testing.T) {
