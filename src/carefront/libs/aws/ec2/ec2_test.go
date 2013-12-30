36322caeec650477ba0a54c620ca454a25aacced
Samuel Stauffer <samuel@carefront.net>
2013-12-30 14:00:12 -0800

    Disable some EC2 tests for now

--- src/carefront/libs/aws/ec2/ec2_test.go
+++ src/carefront/libs/aws/ec2/ec2_test.go
@@ -7,43 +7,43 @@ import (
 )
 
 func TestDescribeSnapshots(t *testing.T) {
-	keys := aws.KeysFromEnvironment()
-	if keys.AccessKey == "" || keys.SecretKey == "" {
-		t.Skip("Skipping aws.ec2 tests. AWS keys not found in environment.")
-	}
-	cli := &aws.Client{
-		Auth: keys,
-	}
-	ec2 := &EC2{
-		Region: aws.USEast,
-		Client: cli,
-	}
-	snaps, err := ec2.DescribeSnapshots(nil, []string{"self"}, nil, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for _, s := range snaps {
-		t.Logf("%+v", s)
-	}
+	// keys := aws.KeysFromEnvironment()
+	// if keys.AccessKey == "" || keys.SecretKey == "" {
+	// 	t.Skip("Skipping aws.ec2 tests. AWS keys not found in environment.")
+	// }
+	// cli := &aws.Client{
+	// 	Auth: keys,
+	// }
+	// ec2 := &EC2{
+	// 	Region: aws.USEast,
+	// 	Client: cli,
+	// }
+	// snaps, err := ec2.DescribeSnapshots(nil, []string{"self"}, nil, nil)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// for _, s := range snaps {
+	// 	t.Logf("%+v", s)
+	// }
 }
 
 func TestDescribeVolumes(t *testing.T) {
-	keys := aws.KeysFromEnvironment()
-	if keys.AccessKey == "" || keys.SecretKey == "" {
-		t.Skip("Skipping aws.ec2 tests. AWS keys not found in environment.")
-	}
-	cli := &aws.Client{
-		Auth: keys,
-	}
-	ec2 := &EC2{
-		Region: aws.USEast,
-		Client: cli,
-	}
-	vols, err := ec2.DescribeVolumes(nil, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for _, v := range vols {
-		t.Logf("%+v %+v", v, v.Attachment)
-	}
+	// keys := aws.KeysFromEnvironment()
+	// if keys.AccessKey == "" || keys.SecretKey == "" {
+	// 	t.Skip("Skipping aws.ec2 tests. AWS keys not found in environment.")
+	// }
+	// cli := &aws.Client{
+	// 	Auth: keys,
+	// }
+	// ec2 := &EC2{
+	// 	Region: aws.USEast,
+	// 	Client: cli,
+	// }
+	// vols, err := ec2.DescribeVolumes(nil, nil)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// for _, v := range vols {
+	// 	t.Logf("%+v %+v", v, v.Attachment)
+	// }
 }
