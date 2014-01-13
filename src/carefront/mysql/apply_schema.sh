3bd3d5eacd00e82c8ba4592fde9c70d3eee2aa2c
Kunal Jham <kunal@carefront.net>
2014-01-13 15:03:52 -0800

    - snapshot production databsae before applying migration DDl

--- src/carefront/mysql/apply_schema.sh
+++ src/carefront/mysql/apply_schema.sh
@@ -42,6 +42,7 @@ do
 			LOGMSG="{\"env\":\"$env\",\"user\":\"$USER\",\"migration_id\":\"$migrationNumber\"}"
 			echo "use $PROD_DB_NAME;" | cat - migration-$migrationNumber.sql > temp.sql
 			scp temp.sql kunal@54.209.10.66:~
+			ssh -t kunal@$PROD_DB_INSTANCE "sudo ec2-consistent-snapshot -mysql.config /mysql-data/mysql/backup.cnf -tag migration-id=migration-$migrationNumber"
 			ssh kunal@54.209.10.66 "mysql -h $PROD_DB_INSTANCE -u $PROD_DB_USER_NAME -p$PROD_DB_PASSWORD < temp.sql ; logger -p user.info -t schema '$LOGMSG'"
 		;;
 	esac
