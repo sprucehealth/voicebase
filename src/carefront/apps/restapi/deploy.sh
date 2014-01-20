7b8b5d67bef3f8f7c6f0fcbbaf2d3e348848313d
Kunal Jham <kunal@carefront.net>
2014-01-20 09:27:34 -0800

    - modifying the deploy script to accomodate the staging environment

--- src/carefront/apps/restapi/deploy.sh
+++ src/carefront/apps/restapi/deploy.sh
@@ -3,40 +3,53 @@
 DATE=$(date +%Y%m%d%H%M)
 DEV_HOSTS="54.209.125.122"
 PROD_HOSTS="10.0.43.95 10.0.95.22"
+STAGING_HOSTS="150.31.9.171 150.31.20.249"
 APP=restapi
-HOSTS=$DEV_HOSTS
 deploy_env=$1
 
 GOVERSION=$(go version)
 REV=$(git rev-parse HEAD)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [ "$deploy_env" == "prod" ]; 
-then
-	# Make sure the current branch is master and is the latest version according to origin/master
+case "$deploy_env" in 
 
-	if [ "$BRANCH" != "master" ]; then
-		echo "Current branch is $BRANCH. Please only deploy from master."
-		exit 2;
-	fi
-	# Pull in latest origin
-	git fetch
-	git diff --quiet origin/master
-	if [ "$?" != "0" ]; then
-		echo "Your repo does not match origin/master. Please make sure there's no uncommited changes and you have the latest changes before deploying."
-		exit 3
-	fi
+	"prod" )
+		HOSTS=$PROD_HOSTS
 
-	read -p "To be sure you want to deploy to production, type PROD if you wish to deploy to production: " confirmation
-	case $confirmation in
-		PROD ) HOSTS=$PROD_HOSTS;;
-		* ) exit;;
-	esac
-elif [ "$deploy_env" != "dev" ];
-then 
-	echo "ERROR: Usage : ./deploy.sh [dev|prod] " >&2
-	exit 1;
-fi
+		# Make sure the current branch is master and is the latest version according to origin/master
+		if [ "$BRANCH" != "master" ]; then
+			echo "Current branch is $BRANCH. Please only deploy from master."
+			exit 2;
+		fi
+
+		# Pull in latest origin
+		git fetch
+		git diff --quiet origin/master
+		if [ "$?" != "0" ]; then
+			echo "Your repo does not match origin/master. Please make sure there's no uncommited changes and you have the latest changes before deploying."
+			exit 3
+		fi
+
+		read -p "To be sure you want to deploy to production, type PROD if you wish to deploy to production: " confirmation
+		case $confirmation in
+			PROD ) HOSTS=$PROD_HOSTS;;
+			* ) exit;;
+		esac
+	;;
+
+	"dev" ) 
+		HOSTS=$DEV_HOSTS
+	;;
+
+	"staging" )
+		HOSTS=$STAGING_HOSTS
+	;;
+
+	* )
+		echo "ERROR: Usage : ./deploy.sh [staging|dev|prod] " >&2
+		exit 1;
+	;;
+esac
 
 set -e
 
