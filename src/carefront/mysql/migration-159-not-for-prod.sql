a7759ded8f44a39673afb114153eb3757b21bafc
Kunal Jham <kunal.jham@gmail.com>
2014-02-14 23:11:07 -0800

    - fix typo

--- src/carefront/mysql/migration-159-not-for-prod.sql
+++ src/carefront/mysql/migration-159-not-for-prod.sql
@@ -1,6 +1,6 @@
-create table dr_demo_whitelist {
+create table dr_demo_whitelist (
 	id int unsigned not null auto_increment,
 	doctor_id int unsigned not null,
 	foreign key (doctor_id) references doctor(id),
 	primary key (id)
-	}
+	) character set utf8;
