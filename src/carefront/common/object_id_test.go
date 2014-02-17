495d5a2444c89e36f590eb24b1b0f863b4a83531
Kunal Jham <kunal.jham@gmail.com>
2014-02-17 14:13:42 -0800

    - adding a new type to marshall/unmarshall ids that represent data   model ids such that a null value or an empty string can be treated as  0 values

---
+++ src/carefront/common/object_id_test.go
@@ -0,0 +1,101 @@
+package common
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+type ExampleObject struct {
+	Id   *ObjectId `json:"testing_id,omitempty"`
+	Name string    `json:"name"`
+}
+
+const (
+	testObjectString = `{
+ "testing_id": "12345",
+ "name": "Hello"
+}`
+	testObjectStringWithEmptyString = `{
+ "testing_id": "",
+ "name": "Hello"
+}`
+
+	testObjectStringWithNull = `{
+ "testing_id": null,
+ "name": "Hello"
+}`
+
+	testObjectStringWithNoObjectId = `{
+ "name": "Hello"
+}`
+)
+
+func TestObjectIdMarshal(t *testing.T) {
+	e1 := &ExampleObject{
+		Id:   &ObjectId{Int64: 12345},
+		Name: "Hello",
+	}
+
+	_, err := json.Marshal(e1)
+	if err != nil {
+		t.Fatal("Unable to marshal objectId as expected: " + err.Error())
+	}
+
+	e2 := &ExampleObject{
+		Name: "Hello",
+	}
+
+	jsonData, err := json.Marshal(e2)
+	if err != nil {
+		t.Fatal("Unable to marshal object with no objectId: " + err.Error())
+	}
+
+	if string(jsonData) != `{"name":"Hello"}` {
+		t.Fatal("ObjectId object did not get marshalled as expected")
+	}
+}
+
+func TestObjectIdUnmarshal(t *testing.T) {
+	testObject := &ExampleObject{}
+	err := json.Unmarshal([]byte(testObjectString), testObject)
+	if err != nil {
+		t.Fatal("Unable to unmarshal object as expected")
+	}
+
+	if testObject.Id.Int64 != 12345 {
+		t.Fatalf("Expected the objectId to be set with 12345. Instead it was set as %d", testObject.Id.Int64)
+	}
+
+	testObject = &ExampleObject{}
+	err = json.Unmarshal([]byte(testObjectStringWithNull), testObject)
+
+	if err != nil {
+		t.Fatal("Unable to unmarshal object as expected: " + err.Error())
+	}
+
+	if testObject.Id != nil {
+		t.Fatalf("Expected the objectId to be set as 0, Instead it was set as %d", testObject.Id.Int64)
+	}
+
+	testObject = &ExampleObject{}
+	err = json.Unmarshal([]byte(testObjectStringWithEmptyString), testObject)
+
+	if err != nil {
+		t.Fatal("Unable to unmarshal object as expected: " + err.Error())
+	}
+
+	if testObject.Id.Int64 != 0 {
+		t.Fatalf("Expected the objectId to be set as 0, Instead it was set as %d", testObject.Id.Int64)
+	}
+
+	testObject = &ExampleObject{}
+	err = json.Unmarshal([]byte(testObjectStringWithNoObjectId), testObject)
+
+	if err != nil {
+		t.Fatal("Unable to unmarshal object as expected")
+	}
+
+	if testObject.Id != nil {
+		t.Fatal("Expected the objectId to be set as 0")
+	}
+}
--- src/carefront/common/utils.go
+++ src/carefront/common/utils.go
@@ -1,12 +1,13 @@
 package common
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-
 	"carefront/libs/aws"
 	"carefront/libs/aws/sqs"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
 	goamz "launchpad.net/goamz/aws"
+	"strconv"
 )
 
 func GenerateToken() (string, error) {
@@ -52,4 +53,29 @@ func NewQueue(auth aws.Auth, region aws.Region, queueName string) (*SQSQueue, er
 		QueueService: sq,
 		QueueUrl:     queueUrl,
 	}, nil
+}
+
+// This is an object used for the (un)marshalling
+// of data models ids, such that null values passed from the client
+// can be treated as 0 values.
+type ObjectId struct {
+	Int64 int64
+}
+
+func (id *ObjectId) UnmarshalJSON(data []byte) error {
+
+	// only treating the case of an empty string or a null value
+	// as value being 0.
+	// otherwise relying on integer parser
+	if len(data) < 2 || string(data) == "null" || string(data) == `""` {
+		return nil
+	}
+
+	intId, err := strconv.ParseInt(string(data[1:len(data)-1]), 10, 64)
+	id.Int64 = intId
+	return err
+}
+
+func (id *ObjectId) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`"%d"`, id.Int64)), nil
 }
