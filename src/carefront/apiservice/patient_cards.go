6a5142f5859326f0021daeb6cd892aed42313754
Kunal Jham <kunal.jham@gmail.com>
2014-02-28 19:23:37 -0800

    - returning card in response to add card

--- src/carefront/apiservice/patient_cards.go
+++ src/carefront/apiservice/patient_cards.go
@@ -26,6 +26,10 @@ type PatientCardsResponse struct {
 	Cards []common.Card `json:"cards"`
 }
 
+type PatientAddCardResponse struct {
+	Card *common.Card `json:"card"`
+}
+
 func (p *PatientCardsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case HTTP_GET:
@@ -315,5 +319,5 @@ func (p *PatientCardsHandler) addCardForPatient(w http.ResponseWriter, r *http.R
 		return
 	}
 
-	WriteJSONToHTTPResponseWriter(w, http.StatusOK, SuccessfulGenericJSONResponse())
+	WriteJSONToHTTPResponseWriter(w, http.StatusOK, PatientAddCardResponse{Card: requestData.Card})
 }
