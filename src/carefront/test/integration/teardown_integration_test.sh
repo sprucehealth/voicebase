fa06ae253ce7dcc29c1e7364be6a3443d37562ac
Kunal Jham <kunal.jham@gmail.com>
2014-03-24 22:19:50 -0700

    not bothering with creation of user instead just using root

--- src/carefront/test/integration/teardown_integration_test.sh
+++ src/carefront/test/integration/teardown_integration_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 echo "drop database $1;" > drop_database.sql
-mysql -h 127.0.0.1 -u travis -p12345 < drop_database.sql
+mysql -h 127.0.0.1 -u root < drop_database.sql
 rm drop_database.sql
