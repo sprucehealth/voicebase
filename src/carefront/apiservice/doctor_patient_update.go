209458f1a09b009b5d237d9fad429bd6aabf6eef
Kunal Jham <kunal.jham@gmail.com>
2014-03-26 23:32:04 -0700

    - getting and updating patient information for unlinked patients

--- src/carefront/apiservice/doctor_patient_update.go
+++ src/carefront/apiservice/doctor_patient_update.go
@@ -130,6 +130,13 @@ func (d *DoctorPatientUpdateHandler) updatePatientInformation(w http.ResponseWri
 
 	trimSpacesFromPatientFields(requestData.Patient)
 
+	// get the erx id for the patient, if it exists in the database
+	existingPatientInfo, err := d.DataApi.GetPatientFromId(requestData.Patient.PatientId.Int64())
+	if err != nil {
+		WriteDeveloperError(w, http.StatusInternalServerError, "Unable to get patient info from database: "+err.Error())
+		return
+	}
+
 	currentDoctor, err := d.DataApi.GetDoctorFromAccountId(GetContext(r).AccountId)
 	if err != nil {
 		WriteDeveloperError(w, http.StatusInternalServerError, "Unable to get doctor from account id: "+err.Error())
