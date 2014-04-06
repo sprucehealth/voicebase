b80b6445ba45c10c2afd9362461ca6853129c022
Kunal Jham <kunal.jham@gmail.com>
2014-04-06 08:31:11 -0700

    debugging

--- src/carefront/test/integration/doctor_treatment_integ_test.go
+++ src/carefront/test/integration/doctor_treatment_integ_test.go
@@ -7,6 +7,7 @@ import (
 	"carefront/encoding"
 	"carefront/libs/erx"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -800,6 +801,8 @@ func addAndGetTreatmentsForPatientVisit(testData TestData, treatments []*common.
 	if err != nil {
 		t.Fatal("Unable to marshal request body for adding treatments to patient visit")
 	}
+
+	fmt.Println(string(data))
 
 	resp, err := authPost(ts.URL, "application/json", bytes.NewBuffer(data), doctorAccountId)
 	if err != nil {
