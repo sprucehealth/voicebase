4688687df499b6a6b217cd5bfb002455b9ba9d6f
Kunal Jham <kunal.jham@gmail.com>
2014-04-16 18:05:35 -0700

    fix typo

--- src/carefront/libs/erx/dosespot_api.go
+++ src/carefront/libs/erx/dosespot_api.go
@@ -140,7 +140,7 @@ type patientStartPrescribingResponse struct {
 type patientUpdate struct {
 	Patient     *patient      `xml:"Patient"`
 	Medications []*medication `xml:"Medications>Medication"`
-	Pharmaces   []*pharmacy   `xml:"Pharmacies>Pharmacy"`
+	Pharmacies  []*pharmacy   `xml:"Pharmacies>Pharmacy"`
 }
 
 type prescription struct {
--- src/carefront/libs/erx/dosespot_service.go
+++ src/carefront/libs/erx/dosespot_service.go
@@ -704,18 +704,18 @@ func (d *DoseSpotService) GetPatientDetails(erxPatientId int64) (*common.Patient
 		})
 	}
 
-	if len(response.PatientUpdates[0].Pharmaces) > 0 {
+	if len(response.PatientUpdates[0].Pharmacies) > 0 {
 		newPatient.Pharmacy = &pharmacySearch.PharmacyData{
 			Source:       pharmacySearch.PHARMACY_SOURCE_SURESCRIPTS,
-			SourceId:     strconv.FormatInt(response.PatientUpdates[0].Pharmaces[0].PharmacyId, 10),
-			Name:         response.PatientUpdates[0].Pharmaces[0].StoreName,
-			AddressLine1: response.PatientUpdates[0].Pharmaces[0].Address1,
-			AddressLine2: response.PatientUpdates[0].Pharmaces[0].Address2,
-			City:         response.PatientUpdates[0].Pharmaces[0].City,
-			State:        response.PatientUpdates[0].Pharmaces[0].State,
-			Postal:       response.PatientUpdates[0].Pharmaces[0].ZipCode,
-			Phone:        response.PatientUpdates[0].Pharmaces[0].PrimaryPhone,
-			Fax:          response.PatientUpdates[0].Pharmaces[0].PrimaryFax,
+			SourceId:     strconv.FormatInt(response.PatientUpdates[0].Pharmacies[0].PharmacyId, 10),
+			Name:         response.PatientUpdates[0].Pharmacies[0].StoreName,
+			AddressLine1: response.PatientUpdates[0].Pharmacies[0].Address1,
+			AddressLine2: response.PatientUpdates[0].Pharmacies[0].Address2,
+			City:         response.PatientUpdates[0].Pharmacies[0].City,
+			State:        response.PatientUpdates[0].Pharmacies[0].State,
+			Postal:       response.PatientUpdates[0].Pharmacies[0].ZipCode,
+			Phone:        response.PatientUpdates[0].Pharmacies[0].PrimaryPhone,
+			Fax:          response.PatientUpdates[0].Pharmacies[0].PrimaryFax,
 		}
 	}
 
