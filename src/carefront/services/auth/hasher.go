1420ac313e1293e7c4d806dca4a9e4b37d229f4c
Samuel Stauffer <samuel@carefront.net>
2014-04-24 15:17:47 -0700

    Add file I forgot about

---
+++ src/carefront/services/auth/hasher.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"code.google.com/p/go.crypto/bcrypt"
+)
+
+const defaultBcryptCost = 10
+
+type PasswordHasher interface {
+	GenerateFromPassword(password []byte) ([]byte, error)
+	CompareHashAndPassword(hashedPassword, password []byte) error
+}
+
+type bcryptHasher struct {
+	cost int
+}
+
+func NewBcryptHasher(cost int) PasswordHasher {
+	if cost < defaultBcryptCost {
+		cost = defaultBcryptCost
+	}
+	return &bcryptHasher{
+		cost: cost,
+	}
+}
+
+func (h *bcryptHasher) GenerateFromPassword(password []byte) ([]byte, error) {
+	return bcrypt.GenerateFromPassword(password, h.cost)
+}
+
+func (h *bcryptHasher) CompareHashAndPassword(hashedPassword, password []byte) error {
+	return bcrypt.CompareHashAndPassword(hashedPassword, password)
+}
