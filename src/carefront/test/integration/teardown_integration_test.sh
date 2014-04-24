0b3f3df798753946a37cd1a82b67f21d582b6e04
Kunal Jham <kunal.jham@gmail.com>
2014-04-24 13:39:28 -0700

    - updating travis file with the localhost as RDS_INSTANCE - also updating setup and teardown to use the RDS_INSTANCE environment   variable

--- src/carefront/test/integration/teardown_integration_test.sh
+++ src/carefront/test/integration/teardown_integration_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 echo "drop database $1;" > drop_database.sql
-mysql -h 127.0.0.1 -u $RDS_USERNAME -p$RDS_PASSWORD < drop_database.sql
+mysql -h $RDS_INSTANCE -u $RDS_USERNAME -p$RDS_PASSWORD < drop_database.sql
 rm drop_database.sql
