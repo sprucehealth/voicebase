f5c14e74656d1f4cc7ebc256f84dd678854d3c66
Kunal Jham <kunal.jham@gmail.com>
2014-06-04 16:56:53 -0700

    @samuel FYI on migration fix

--- src/carefront/mysql/migration-238.sql
+++ src/carefront/mysql/migration-238.sql
@@ -1,6 +1,6 @@
 alter table treatment_plan add column patient_id int unsigned;
 update treatment_plan 
-	inner join patient_visit on patient_visit_id = patient_visit.patient_id
+	inner join patient_visit on patient_visit_id = patient_visit.id
 	set treatment_plan.patient_id = patient_visit.patient_id;
 alter table treatment_plan modify column patient_id int unsigned not null;
 alter table treatment_plan add foreign key (patient_id) references patient(id);
