0e1afa651d21124a47419ddc4526db733e0d9d77
Kunal Jham <kunal.jham@gmail.com>
2014-06-18 21:30:19 -0700

    - removing attempting to get hte primary doctor to fill in the doctor   name into the question title `

--- src/carefront/patient_visit/patient_visit.go
+++ src/carefront/patient_visit/patient_visit.go
@@ -3,7 +3,6 @@ package patient_visit
 import (
 	"carefront/api"
 	"carefront/apiservice"
-	"carefront/common"
 	"carefront/info_intake"
 	"carefront/libs/dispatch"
 	"net/http"
@@ -125,24 +124,7 @@ func (s *patientVisitHandler) returnLastCreatedPatientVisit(w http.ResponseWrite
 		return
 	}
 
-	careTeam, err := s.dataApi.GetCareTeamForPatient(patientId)
-	if err != nil {
-		apiservice.WriteDeveloperError(w, http.StatusInternalServerError, "Unable to get care team for patient")
-		return
-	}
-
-	primaryDoctorId := apiservice.GetPrimaryDoctorIdFromCareTeam(careTeam)
-	if primaryDoctorId == 0 {
-		apiservice.WriteDeveloperError(w, http.StatusInternalServerError, "Unable to identify the primary doctor for the patient")
-		return
-	}
-	doctor, err := s.dataApi.GetDoctorFromId(primaryDoctorId)
-	if err != nil {
-		apiservice.WriteDeveloperError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	patientVisitLayout, err := GetPatientVisitLayout(s.dataApi, patientId, patientVisitId, r, doctor)
+	patientVisitLayout, err := GetPatientVisitLayout(s.dataApi, patientId, patientVisitId, r)
 	if err != nil {
 		apiservice.WriteDeveloperError(w, http.StatusInternalServerError, err.Error())
 		return
@@ -151,7 +133,7 @@ func (s *patientVisitHandler) returnLastCreatedPatientVisit(w http.ResponseWrite
 	apiservice.WriteJSONToHTTPResponseWriter(w, http.StatusOK, PatientVisitResponse{PatientVisitId: patientVisit.PatientVisitId.Int64(), Status: patientVisit.Status, ClientLayout: patientVisitLayout})
 }
 
-func GetPatientVisitLayout(dataApi api.DataAPI, patientId, patientVisitId int64, r *http.Request, doctor *common.Doctor) (*info_intake.InfoIntakeLayout, error) {
+func GetPatientVisitLayout(dataApi api.DataAPI, patientId, patientVisitId int64, r *http.Request) (*info_intake.InfoIntakeLayout, error) {
 
 	// if there is an active patient visit record, then ensure to lookup the layout to send to the patient
 	// based on what layout was shown to the patient at the time of opening of the patient visit, NOT the current
@@ -172,7 +154,6 @@ func GetPatientVisitLayout(dataApi api.DataAPI, patientId, patientVisitId int64,
 	if err != nil {
 		return nil, err
 	}
-	fillInFormattedFieldsForQuestions(patientVisitLayout, doctor)
 	return patientVisitLayout, nil
 }
 
@@ -208,18 +189,11 @@ func (s *patientVisitHandler) createNewPatientVisitHandler(w http.ResponseWriter
 		return
 	}
 
-	doctor, err := apiservice.GetPrimaryDoctorInfoBasedOnPatient(s.dataApi, patient, "")
-	if err != nil {
-		apiservice.WriteDeveloperError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
 	err = populateGlobalSectionsWithPatientAnswers(s.dataApi, healthCondition, patient.PatientId.Int64(), r)
 	if err != nil {
 		apiservice.WriteDeveloperError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	fillInFormattedFieldsForQuestions(healthCondition, doctor)
 
 	dispatch.Default.PublishAsync(&VisitStartedEvent{
 		PatientId: patient.PatientId.Int64(),
