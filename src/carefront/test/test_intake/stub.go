eef06bd3e464976ca676e45aeff4edb67b9d07c4
Samuel Stauffer <samuel@carefront.net>
2014-06-24 09:47:30 -0700

    Add empty files to integration tests to make the packages compile

---
+++ src/carefront/test/test_intake/stub.go
@@ -0,0 +1,4 @@
+package test_intake
+
+// This file exists to silence the compiler since this
+// package contains no non-test files.
---
+++ src/carefront/test/test_treatment_plan/stub.go
@@ -0,0 +1,4 @@
+package test_treatment_plan
+
+// This file exists to silence the compiler since this
+// package contains no non-test files.
