6709954a1a31acf4b5fda48de184f993cac6631f
Samuel Stauffer <samuel@descolada.com>
2014-06-26 18:46:42 -0700

    Fix version info in build script

--- apps/restapi/build.sh
+++ apps/restapi/build.sh
@@ -13,11 +13,11 @@ TIME=$(date)
 LATEST_MIGRATION=$(ls -r ../../mysql/snapshot-*.sql | cut -d- -f 2  | cut -d. -f1 | sort -nr | head -1)
 GOOS=linux GOARCH=amd64 \
 	go build -ldflags " \
-		-X carefront/common/config.GitRevision '$REV' \
-		-X carefront/common/config.GitBranch '$BRANCH' \
-		-X carefront/common/config.BuildTime '$TIME' \
-		-X carefront/common/config.BuildNumber '$TRAVIS_BUILD_NUMBER' \
-		-X carefront/common/config.MigrationNumber '$LATEST_MIGRATION'" -o $APP
+		-X github.com/sprucehealth/backend/common/config.GitRevision '$REV' \
+		-X github.com/sprucehealth/backend/common/config.GitBranch '$BRANCH' \
+		-X github.com/sprucehealth/backend/common/config.BuildTime '$TIME' \
+		-X github.com/sprucehealth/backend/common/config.BuildNumber '$TRAVIS_BUILD_NUMBER' \
+		-X github.com/sprucehealth/backend/common/config.MigrationNumber '$LATEST_MIGRATION'" -o $APP
 
 # Embed resources in the binary
 
