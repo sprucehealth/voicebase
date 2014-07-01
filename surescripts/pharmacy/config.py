a1bcc46a8681c2fbf8b97cb9a5b1a0deb445ea74
Kunal Jham <kunal.jham@gmail.com>
2014-07-01 10:26:11 -0700

    - reading auth token and id for smarty streets from environment   variables

--- surescripts/pharmacy/config.py
+++ surescripts/pharmacy/config.py
@@ -5,14 +5,14 @@ You may want to use environment variables or some other mechanism. Be our guest.
 
 
 import urllib
+import os
 
-
-# TODO: copy your auth-id and auth-token here!
-# (the auth-id and auth-token below are invalid)
+smartyStreetsAuthId = os.environment['SMARTY_STREETS_AUTH_ID']
+smartyStreetsAuthToken = os.environment['SMARTY_STREETS_AUTH_TOKEN']
 
 AUTHENTICATION = {
-    'auth-id': '84a88b2b-acc0-402b-8b67-c6af09e6080a',
+    'auth-id': smartyStreetsAuthId,
     'auth-token': urllib.unquote(
-        'M9rSnFoqXJmI71YaKAU2D3DpVAjJhBXDTPjWSUwdGl2iPkkDFIdTJrTrCBrrv1WufIeDJNkC68rP%2FQo8iKOUng%3D%3D'
+        smartyStreetsAuthToken
     ),
 }
