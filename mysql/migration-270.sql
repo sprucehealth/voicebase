cb6d409ba755ce7403641c8d9e3af0cc00ae5681
Kunal Jham <kunal.jham@gmail.com>
2014-07-10 15:24:03 -0700

    - setting the modified_date to set the default time if no value   specified - ensuring that expires column is nullable

---
+++ mysql/migration-270.sql
@@ -0,0 +1,3 @@
+alter table dr_regimen_step modify column modified_date timestamp(6) not null default current_timestamp(6) on update current_timestamp(6);
+alter table dr_advice_point modify column modified_date timestamp(6) not null default current_timestamp(6) on update current_timestamp(6);
+alter table unclaimed_case_queue modify column expires timestamp(6);
