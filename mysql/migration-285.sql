080a465bbde763ed60d99b2e6b083fabac4747a5
Kunal Jham <kunal.jham@gmail.com>
2014-07-31 17:04:32 -0700

    moving the non-nullable columsn changes to be the last migration

---
+++ mysql/migration-285.sql
@@ -0,0 +1,6 @@
+alter table doctor modify column dob_day int unsigned not null;
+alter table doctor modify column dob_month int unsigned not null;
+alter table doctor modify column dob_year int unsigned not null;
+alter table patient modify column dob_day int unsigned not null;
+alter table patient modify column dob_month int unsigned not null;
+alter table patient modify column dob_year int unsigned not null;
