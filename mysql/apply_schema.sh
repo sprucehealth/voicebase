c94ef5298491bbcddd24d504d8f862a826f4ccd9
Samuel Stauffer <samuel@descolada.com>
2014-08-04 10:52:49 -0700

    Warn in apply_schema when the appropriate env vars aren't set

--- mysql/apply_schema.sh
+++ mysql/apply_schema.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -e
 
-# This script makes it easy to apply changes to the development and production database once 
-# the schema has been validated. 
+# This script makes it easy to apply changes to the development and production database once
+# the schema has been validated.
 
 LOCAL_DB_USERNAME="carefront"
 LOCAL_DB_NAME="carefront_db"
 RDS_USERNAME="carefront"
 RDS_DB_NAME="carefront_db"
+PROD_DB_NAME="carefront"
+PROD_DB_INSTANCE="master.mysql.service.prod-us-east-1.spruce"
 
-argsArray=($@) 
+argsArray=($@)
 len=${#argsArray[@]}
 
 if [ $len -lt 2 ];
@@ -19,24 +21,24 @@ fi
 
 env=${argsArray[0]}
 for migrationNumber in ${argsArray[@]:1:$len}
-do 
+do
 	echo "Applying migration-$migrationNumber.sql"
-	
+
 	# ensure that the file exists
 	if [ ! -f snapshot-$migrationNumber.sql ] || [ ! -f data-snapshot-$migrationNumber.sql ] || [ ! -f migration-$migrationNumber.sql ]; then
 		echo "ERROR: Looks like migration $migrationNumber has not yet been validated using validate_schema.sql and so they will not be applied to database"
 		exit 1
 	fi
 
-	
+
 	case "$env" in
-		
+
 		"local" )
 			echo "use $LOCAL_DB_NAME; insert into migrations (migration_id, migration_user) values ($migrationNumber, '$USER');" > temp-migration.sql
 			echo "use $LOCAL_DB_NAME;" | cat - migration-$migrationNumber.sql > temp.sql
 			mysql -u $LOCAL_DB_USERNAME -p$LOCAL_DB_PASSWORD < temp.sql
 			mysql -u $LOCAL_DB_USERNAME -p$LOCAL_DB_PASSWORD < temp-migration.sql
-		;;	
+		;;
 
 		"staging" )
 			echo "use $STAGING_DB_NAME; insert into migrations (migration_id, migration_user) values ($migrationNumber, '$USER');" > temp-migration.sql
@@ -55,7 +57,7 @@ do
 			mysql -h $DEV_RDS_INSTANCE -u $RDS_USERNAME -p$DEV_RDS_PASSWORD < temp-migration.sql
 		;;
 
-		"demo" ) 
+		"demo" )
 			echo "use $DEMO_DB_NAME; insert into migrations (migration_id, migration_user) values ($migrationNumber, '$USER');" > temp-migration.sql
 			LOGMSG="{\"env\":\"$env\",\"user\":\"$USER\",\"migration_id\":\"$migrationNumber\"}"
 			echo "use $DEMO_DB_NAME;" | cat - migration-$migrationNumber.sql > temp.sql
@@ -63,8 +65,16 @@ do
 			scp temp-migration.sql 54.210.97.69:~
 			ssh 54.210.97.69 "mysql -u $DEMO_DB_USER_NAME -p$DEMO_DB_PASSWORD < temp.sql ; mysql -u $DEMO_DB_USER_NAME -p$DEMO_DB_PASSWORD < temp-migration.sql; logger -p user.info -t schema '$LOGMSG'"
 		;;
-		
-		"prod" ) 
+
+		"prod" )
+			if [ "$PROD_DB_USER_NAME" == "" ]; then
+				echo "PROD_DB_USER_NAME not set"
+				exit 1
+			fi
+			if [ "$PROD_DB_PASSWORD" == "" ]; then
+				echo "PROD_DB_PASSWORD not set"
+				exit 1
+			fi
 			echo "use $PROD_DB_NAME; insert into migrations (migration_id, migration_user) values ($migrationNumber, '$USER');" > temp-migration.sql
 			LOGMSG="{\"env\":\"$env\",\"user\":\"$USER\",\"migration_id\":\"$migrationNumber\"}"
 			echo "use $PROD_DB_NAME;" | cat - migration-$migrationNumber.sql > temp.sql
@@ -74,7 +84,7 @@ do
 			ssh 54.209.10.66 "mysql -h $PROD_DB_INSTANCE -u $PROD_DB_USER_NAME -p$PROD_DB_PASSWORD < temp.sql ; mysql -h $PROD_DB_INSTANCE -u $PROD_DB_USER_NAME -p$PROD_DB_PASSWORD < temp-migration.sql; logger -p user.info -t schema '$LOGMSG'"
 		;;
 	esac
-	
+
 	rm temp.sql temp-migration.sql
 
 done
