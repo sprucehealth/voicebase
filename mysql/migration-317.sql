80443e816cee3aa238e3560f37da3f14de8e14d1
Kunal Jham <kunal.jham@gmail.com>
2014-09-03 11:47:09 -0700

    removing mysql statement to create template

--- mysql/migration-317.sql
+++ mysql/migration-317.sql
@@ -1,18 +1,3 @@
-create table scheduled_message (
-	id int unsigned not null auto_increment,
-	patient_id int unsigned not null,
-	message_type varchar(64) not null,
-	message_json blob not null,
-	type varchar(64) not null,
-	status varchar(64) not null,
-	created timestamp not null default current_timestamp,
-	scheduled timestamp default current_timestamp,
-	completed timestamp,
-	error varchar(512),
-	foreign key (patient_id) references patient(id),
-	primary key (id)
-) character set utf8;
-
 create table scheduled_message_template (
 	id int unsigned not null auto_increment,
 	type varchar(64) not null,
@@ -24,10 +9,3 @@ create table scheduled_message_template (
 	foreign key (creator_account_id) references account(id),
 	primary key (id)
 ) character set utf8;
-
-insert into scheduled_message_template (type, schedule_period, app_message_template_json, creator_account_id) 
-	VALUES ('treatment_plan_viewed', 2, '
-			{
-				"SenderRole" : "MA",
-				"Message" : "Hi [Patient.FirstName], Did you pick up your prescriptions? Thanks, [Provider.ShortDisplayName]"
-				}', 94);
