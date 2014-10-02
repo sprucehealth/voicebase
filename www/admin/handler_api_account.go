33577a5fc9aa6eb930de45ec219fdcb1f974e8da
Kunal Jham <kunal.jham@gmail.com>
2014-10-02 12:02:14 -0700

    fixes to ensure that anyone with doctor permissions can view and edit ma and doctor role

--- www/admin/handler_api_account.go
+++ www/admin/handler_api_account.go
@@ -91,7 +91,8 @@ func (h *accountHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func accountReadAccess(account *common.Account, perms www.Permissions) bool {
-	if perms.Has(PermDoctorsView) && account.Role == api.DOCTOR_ROLE {
+	if perms.Has(PermDoctorsView) &&
+		(account.Role == api.DOCTOR_ROLE || account.Role == api.MA_ROLE) {
 		return true
 	}
 	if perms.Has(PermAdminAccountsView) && account.Role == api.ADMIN_ROLE {
@@ -101,7 +102,8 @@ func accountReadAccess(account *common.Account, perms www.Permissions) bool {
 }
 
 func accountWriteAccess(account *common.Account, perms www.Permissions) bool {
-	if perms.Has(PermDoctorsEdit) && account.Role == api.DOCTOR_ROLE {
+	if perms.Has(PermDoctorsEdit) &&
+		(account.Role == api.DOCTOR_ROLE || account.Role == api.MA_ROLE) {
 		return true
 	}
 	if perms.Has(PermAdminAccountsEdit) && account.Role == api.ADMIN_ROLE {
