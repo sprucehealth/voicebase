4b5d078bf20a57c34b1090c159cf0b4ea1eeae86
Kunal Jham <kunal.jham@gmail.com>
2014-11-26 16:24:48 -0800

    @samuel @sibljon changing the environment variables to run the test locally so that the environment variables aren't referring to an rds instance

--- test/test_integration/setup_integration_test.sh
+++ test/test_integration/setup_integration_test.sh
@@ -5,11 +5,11 @@ MYSQL_FOLDER=${GOPATH}/src/github.com/sprucehealth/backend/mysql
 pushd $MYSQL_FOLDER > /dev/null
 latestSnapshotNumber=`ls -r snapshot-*.sql | cut -d- -f 2  | cut -d. -f1 | sort -nr | head -1`
 latestDataSnapshotNumber=`ls -r data-snapshot-*.sql | cut -d- -f 3  | cut -d. -f1 | sort -nr | head -1`
-PASSWORD_ARG="-p$RDS_PASSWORD"
-if [ "$RDS_PASSWORD" = "" ]; then
+PASSWORD_ARG="-p$CF_LOCAL_DB_PASSWORD"
+if [ "$CF_LOCAL_DB_PASSWORD" = "" ]; then
 	PASSWORD_ARG=""
 fi
-echo "create database $TEST_DB; use $TEST_DB;"  | cat - snapshot-$latestSnapshotNumber.sql | mysql -h $RDS_INSTANCE -u $RDS_USERNAME $PASSWORD_ARG
-echo "use $TEST_DB;" | cat - data-snapshot-$latestDataSnapshotNumber.sql | mysql -h $RDS_INSTANCE -u $RDS_USERNAME $PASSWORD_ARG
+echo "create database $TEST_DB; use $TEST_DB;"  | cat - snapshot-$latestSnapshotNumber.sql | mysql -h $CF_LOCAL_DB_INSTANCE -u $CF_LOCAL_DB_USERNAME $PASSWORD_ARG
+echo "use $TEST_DB;" | cat - data-snapshot-$latestDataSnapshotNumber.sql | mysql -h $CF_LOCAL_DB_INSTANCE -u $CF_LOCAL_DB_USERNAME $PASSWORD_ARG
 popd > /dev/null
 echo $TEST_DB
