0003e5494555c66e5bb0a8ac6002acc242deec51
Kunal Jham <kunal.jham@gmail.com>
2014-11-26 16:28:13 -0800

    updating teardown

--- test/test_integration/teardown_integration_test.sh
+++ test/test_integration/teardown_integration_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PASSWORD_ARG="-p$RDS_PASSWORD"
-if [ "$RDS_PASSWORD" = "" ]; then
+PASSWORD_ARG="-p$CF_LOCAL_DB_PASSWORD"
+if [ "$CF_LOCAL_DB_PASSWORD" = "" ]; then
 	PASSWORD_ARG=""
 fi
-echo "drop database $1;" | mysql -h $RDS_INSTANCE -u $RDS_USERNAME $PASSWORD_ARG
+echo "drop database $1;" | mysql -h $CF_LOCAL_DB_INSTANCE -u $CF_LOCAL_DB_USERNAME $PASSWORD_ARG
