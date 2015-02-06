744c5bea2a0a514997b07102a0539647fa09545a
Michael Raines <mike.g.raines@gmail.com>
2015-02-06 14:03:46 -0800

    Fix go vet error

--- responses/care_provider.go
+++ responses/care_provider.go
@@ -10,7 +10,7 @@ type CareProvider struct {
 	ProviderID       int64  `json:"provider_id,string"`
 	FirstName        string `json:"first_name,omitempty"`
 	LastName         string `json:"last_name,omitempty"`
-	ShortTitle       string `json:"short_title,omitempty`
+	ShortTitle       string `json:"short_title,omitempty"`
 	LongTitle        string `json:"long_title,omitempty"`
 	ShortDisplayName string `json:"short_display_name,omitempty"`
 	LongDisplayName  string `json:"long_display_name,omitempty"`
