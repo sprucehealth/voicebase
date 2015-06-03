b2e86da6a98e488c00d4f20ce74212af4303da6b
Samuel Stauffer <samuel@sprucehealth.com>
2015-06-03 11:17:04 -0700

    Include raw coverage data with builds

--- docker-ci/run.sh
+++ docker-ci/run.sh
@@ -91,7 +91,8 @@ if [[ "$DEPLOY_TO_S3" != "" ]]; then
     cp restapi build/$CMD_NAME
     bzip2 -9 build/$CMD_NAME
     echo $GIT_COMMIT > build/$CMD_NAME.revision
-    cp ../../coverage.html build/$CMD_NAME.coverage.html
+    cp ../../coverage-$BUILD_NUMBER.out build/$CMD_NAME.coverage
+    cp ../../coverage-$BUILD_NUMBER.html build/$CMD_NAME.coverage.html
     s3cmd --add-header "x-amz-acl:bucket-owner-full-control" -M --server-side-encryption put build/* s3://spruce-deploy/restapi/
 
     cd ../../resources/static
