d65411f8b3c0584cff6aa012f76d1e6ebccb915a
Samuel Stauffer <samuel@sprucehealth.com>
2015-08-01 11:13:53 -0700

    Require patient role for consent handler

--- www/home/handler_parental_consent.go
+++ www/home/handler_parental_consent.go
@@ -75,12 +75,15 @@ func checkParentalConsentAccessToken(w http.ResponseWriter, r *http.Request, dat
 }
 
 func newParentalConsentHandler(dataAPI api.DataAPI, mediaStore *media.Store, templateLoader *www.TemplateLoader) httputil.ContextHandler {
-	return httputil.ContextSupportedMethods(&parentalConsentHandler{
-		dataAPI:         dataAPI,
-		mediaStore:      mediaStore,
-		template:        templateLoader.MustLoadTemplate("home/parental-consent.html", "", nil),
-		landingTemplate: templateLoader.MustLoadTemplate("home/parental-landing.html", "home/parental-base.html", nil),
-	}, httputil.Get)
+	return httputil.ContextSupportedMethods(
+		www.RoleRequiredHandler(
+			&parentalConsentHandler{
+				dataAPI:         dataAPI,
+				mediaStore:      mediaStore,
+				template:        templateLoader.MustLoadTemplate("home/parental-consent.html", "", nil),
+				landingTemplate: templateLoader.MustLoadTemplate("home/parental-landing.html", "home/parental-base.html", nil),
+			}, nil, api.RolePatient),
+		httputil.Get)
 }
 
 func (h *parentalConsentHandler) ServeHTTP(ctx context.Context, w http.ResponseWriter, r *http.Request) {
