c2358c8e12690cda9a60e3293a2799041994f70a
Samuel Stauffer <samuel@sprucehealth.com>
2015-08-19 09:58:42 -0700

    Fix migration 437

--- mysql/migration-437.sql
+++ mysql/migration-437.sql
@@ -1,4 +1,4 @@
 -- Copy update for Parental Consent flow
 UPDATE localized_text
-SET ltext = "Hey, I’d like to see a dermatologist for my acne. With Spruce I can see a board-certified dermatologist but need your approval before a doctor can treat me (since it’s remote treatment). Here’s the link %s - all the info you need should be there along with contact details for Spruce."
+SET ltext = 'Hey, I''d like to see a dermatologist for my acne. With Spruce I can see a board-certified dermatologist but need your approval before a doctor can treat me (since it’s remote treatment). Here’s the link %s - all the info you need should be there along with contact details for Spruce.'
 WHERE app_text_id = (SELECT id FROM app_text WHERE app_text_tag = 'parental_consent_request_sms');
