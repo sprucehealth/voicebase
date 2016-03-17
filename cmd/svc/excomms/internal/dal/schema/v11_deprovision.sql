627795200da00de26ad35458b9013945a99857da
Kunal Jham <kunal@sprucehealth.com>
2016-03-16 20:41:00 -0700

    fix typo

--- cmd/svc/excomms/internal/dal/schema/v11_deprovision.sql
+++ cmd/svc/excomms/internal/dal/schema/v11_deprovision.sql
@@ -1,3 +1,3 @@
 ALTER TABLE provisioned_endpoint ADD COLUMN deprovisioned TINYINT(1) NOT NULL DEFAULT 0;
 ALTER TABLE provisioned_endpoint ADD COLUMN deprovisioned_timestamp TIMESTAMP;
-ALTER TABLE provisioned_endpoint ADD COLUMN deprovision_reason VARCHAR(255);
+ALTER TABLE provisioned_endpoint ADD COLUMN deprovisioned_reason VARCHAR(255);
