05e3c4a6c71f6dec97f496b7e19d804562827813
Samuel Stauffer <samuel@sprucehealth.com>
2016-03-24 19:38:19 -0700

    Update threads table migration

--- cmd/svc/threading/schema/v12_internal_thread.sql
+++ cmd/svc/threading/schema/v12_internal_thread.sql
@@ -1,6 +1,8 @@
 ALTER TABLE threads ADD COLUMN system_title VARCHAR(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 ALTER TABLE threads ADD COLUMN user_title VARCHAR(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 ALTER TABLE threads ADD COLUMN type VARCHAR(64) CHARACTER SET ascii COLLATE ascii_bin;
+UPDATE threads SET type = '' WHERE type is NULL;
+ALTER TABLE threads MODIFY COLUMN type VARCHAR(64) CHARACTER SET ascii COLLATE ascii_bin NOT NULL;
 ALTER TABLE thread_members RENAME thread_entities;
 ALTER TABLE thread_entities DROP COLUMN following;
 ALTER TABLE thread_entities ADD COLUMN member BOOLEAN NOT NULL DEFAULT false;
