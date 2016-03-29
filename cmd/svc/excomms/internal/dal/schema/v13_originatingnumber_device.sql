b5cf9f0b57060256a18ff71c8a1b45e0e18d24cc
Kunal Jham <kunal@sprucehealth.com>
2016-03-29 14:23:41 -0700

    Updating the DML to ensure that we are looking at entity_id and device_id pair as primary key

--- cmd/svc/excomms/internal/dal/schema/v13_originatingnumber_device.sql
+++ cmd/svc/excomms/internal/dal/schema/v13_originatingnumber_device.sql
@@ -1,1 +1,4 @@
 ALTER TABLE originating_phone_number ADD COLUMN device_id VARCHAR(128);
+ALTER TABLE originating_phone_number DROP PRIMARY KEY;
+UPDATE originating_phone_number SET device_id='' WHERE device_id is NULL;
+ALTER TABLE originating_phone_number ADD PRIMARY KEY (entity_id, device_id);
