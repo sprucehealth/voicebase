473601f1d436fae84b96a2e69d7f5b3a20fd7fc7
Kunal Jham <kunal@sprucehealth.com>
2016-04-08 14:26:31 -0700

    Script to migrate cases from one doctor to another on a state by state basis or for all states
    Reviewers: michaelraines, samuel
    
    Reviewed By: samuel
    
    Subscribers: janky
    
    Differential Revision: https://phabricator.corp.sprucehealth.com/D611

---
+++ cmd/cli/doctormigration/doc.go
@@ -0,0 +1,3 @@
+// The purpose of this script is to mass migrate patient cases in a provided list of states
+// from one doctor to another, and inform patients via a scheduled message of the change in their doctor.
+package main
