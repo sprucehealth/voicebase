37ed4dcef227574439c9208ca9282f0f6cf408d4
Kunal Jham <kunal@sprucehealth.com>
2016-04-13 14:32:55 -0700

    Remove the defaulting to false for prod for team conversations

--- cmd/svc/baymaxgraphql/main.go
+++ cmd/svc/baymaxgraphql/main.go
@@ -135,8 +135,6 @@ func main() {
 
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 
-	// keep team conversations off by default in prod, but on in non-prod
-	baymaxgraphqlsettings.TeamConversationsConfig.GetBoolean().Default.Value = !environment.IsProd()
 	_, err = settings.RegisterConfigs(
 		ctx,
 		settingsClient,
