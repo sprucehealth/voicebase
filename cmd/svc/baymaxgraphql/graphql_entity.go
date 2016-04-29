e84769bd101b10d6b0b39e13081bd8819baf9ba7
Michael Raines <michael@sprucehealth.com>
2016-04-29 16:46:39 -0700

    support node patient entity
    Reviewers: kunaljham, samuel
    
    Reviewed By: samuel
    
    Subscribers: janky
    
    Differential Revision: https://phabricator.corp.sprucehealth.com/D678

--- cmd/svc/baymaxgraphql/graphql_entity.go
+++ cmd/svc/baymaxgraphql/graphql_entity.go
@@ -164,7 +164,7 @@ func lookupEntity(ctx context.Context, svc *service, ram raccess.ResourceAccesso
 			}
 		}
 		return org, nil
-	case directory.EntityType_INTERNAL, directory.EntityType_EXTERNAL, directory.EntityType_SYSTEM:
+	case directory.EntityType_INTERNAL, directory.EntityType_EXTERNAL, directory.EntityType_SYSTEM, directory.EntityType_PATIENT:
 		e, err := transformEntityToResponse(svc.staticURLPrefix, em, sh, gqlctx.Account(ctx))
 		if err != nil {
 			return nil, errors.InternalError(ctx, err)
