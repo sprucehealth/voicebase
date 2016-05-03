288bce5b178b068b23e17ba4c13fce9188606849
Samuel Stauffer <samuel@sprucehealth.com>
2016-05-03 14:17:46 -0700

    Update setup thread migration

--- cmd/svc/threading/schema/v15_onboarding_state.sql
+++ cmd/svc/threading/schema/v15_onboarding_state.sql
@@ -3,3 +3,5 @@ CREATE INDEX threads_type_deleted ON threads (organization_id, type, deleted);
 
 -- Delete old setup threads, will be recreated through a migration cli
 UPDATE threads SET deleted = 1 WHERE type = 'SETUP';
+-- There's a unique key on the entity_id in onboarding_threads so need to remove the old rows
+DELETE FROM onboarding_threads;
