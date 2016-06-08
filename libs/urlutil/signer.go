0fcf54c63c419b25ed44966dc4ca1a88fd753a84
Kunal Jham <kunal@sprucehealth.com>
2016-06-08 09:59:47 -0700

    remove printline

--- libs/urlutil/signer.go
+++ libs/urlutil/signer.go
@@ -42,7 +42,6 @@ func (s *Signer) SignedURL(uPath string, params url.Values, expires *time.Time) 
 	if err != nil {
 		return "", err
 	}
-	fmt.Println(base64.URLEncoding.EncodeToString(sig))
 	params.Set(SigParamName, base64.URLEncoding.EncodeToString(sig))
 	// doing a path.join helps take care of any leading forward slashes in the path
 	return s.baseURL + path.Join("/", uPath) + "?" + params.Encode(), nil
