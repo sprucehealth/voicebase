f8bf3b275a82856012da470f856004ab6897b84c
Kunal Jham <kunal@sprucehealth.com>
2016-07-11 21:09:11 -0700

    fix apple deep link url

--- cmd/svc/invite/internal/handlers/apple_deeplink.go
+++ cmd/svc/invite/internal/handlers/apple_deeplink.go
@@ -11,38 +11,48 @@ import (
 type appleDeeplinkHandler struct {
 }
 
+type appDetails struct {
+	AppID string   `json:"appID"`
+	Paths []string `json:"paths"`
+}
+type appLinks struct {
+	Apps    []interface{} `json:"apps"`
+	Details []appDetails  `json:"details"`
+}
+
+type appLinksContainer struct {
+	AppLinks appLinks `json:"appLinks"`
+}
+
 func (*appleDeeplinkHandler) ServeHTTP(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 
 	switch environment.GetCurrent() {
 	case environment.Prod:
-		httputil.JSONResponse(w, http.StatusOK, `{
-      "applinks": {
-        "apps": [],
-        "details": [
-          {
-            "appID": "VASUED9B9G.com.sprucehealth.messenger-live",
-            "paths": [
-              "*"
-            ]
-          }
-        ]
-      }
-    }`)
+		httputil.JSONResponse(w, http.StatusOK, appLinksContainer{
+			AppLinks: appLinks{
+				Apps: []interface{}{},
+				Details: []appDetails{
+					{
+						AppID: "VASUED9B9G.com.sprucehealth.messenger-live",
+						Paths: []string{"*"},
+					},
+				},
+			},
+		})
+
 		return
 	case environment.Staging:
-		httputil.JSONResponse(w, http.StatusOK, `{
-      "applinks": {
-        "apps": [],
-        "details": [
-          {
-            "appID": "PRKG6MA37R.com.sprucehealth.messenger-staging",
-            "paths": [
-              "*"
-            ]
-          }
-        ]
-      }
-    }`)
+		httputil.JSONResponse(w, http.StatusOK, appLinksContainer{
+			AppLinks: appLinks{
+				Apps: []interface{}{},
+				Details: []appDetails{
+					{
+						AppID: "PRKG6MA37R.com.sprucehealth.messenger-staging",
+						Paths: []string{"*"},
+					},
+				},
+			},
+		})
 		return
 	}
 }
