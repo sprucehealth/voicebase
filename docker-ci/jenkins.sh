e8a4768a288f5acd5426927e8c081aa6815b7a79
Kunal Jham <kunal@sprucehealth.com>
2016-08-26 15:06:01 -0700

    adding deployable for patientsync-api

--- docker-ci/jenkins.sh
+++ docker-ci/jenkins.sh
@@ -73,7 +73,7 @@ devDeployableMap["care"]="deployable_0E91VDOHG0O00"
 devDeployableMap["media"]="deployable_0EM1LSDHG0O00,deployable_0EP10F1PG0O00"
 devDeployableMap["admin"]="deployable_0FRP18V1G0O00"
 devDeployableMap["payments"]="deployable_0G6QO9CUO0O00"
-devDeployableMap["patientsync"]="deployable_0GGFHCRPO0O00"
+devDeployableMap["patientsync"]="deployable_0GGFHCRPO0O00,deployable_0GGFI5HIO0O00"
 
 if [[ "$DEPLOY_TO_S3" != "" ]]; then
 	echo "Pushing images for revision: $GIT_REV"
