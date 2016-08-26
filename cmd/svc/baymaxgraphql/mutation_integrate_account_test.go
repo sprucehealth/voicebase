bc2b067143a3e4187e0be80b5ba4fc822dacbefc
Michael Raines <michael@sprucehealth.com>
2016-08-26 12:50:51 -0700

    Assert existance and org on connect account
    Reviewers: samuel, kunaljham
    
    Reviewed By: kunaljham
    
    Subscribers: janky
    
    Differential Revision: https://phabricator.corp.sprucehealth.com/D934

--- cmd/svc/baymaxgraphql/mutation_integrate_account_test.go
+++ cmd/svc/baymaxgraphql/mutation_integrate_account_test.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 
 	"github.com/sprucehealth/backend/cmd/svc/baymaxgraphql/internal/raccess"
 	ramock "github.com/sprucehealth/backend/cmd/svc/baymaxgraphql/internal/raccess/mock"
 	"github.com/sprucehealth/backend/libs/test"
 	"github.com/sprucehealth/backend/libs/testhelpers/mock"
+	"github.com/sprucehealth/backend/svc/directory"
 	"github.com/sprucehealth/backend/svc/patientsync"
 	"github.com/sprucehealth/backend/svc/payments"
 	"github.com/sprucehealth/graphql"
@@ -27,6 +32,76 @@ func TestConnectVendorStripeAccount(t *testing.T) {
 		Expected    interface{}
 		ExpectedErr error
 	}{
+		"Error-NotFound-NoEntity": {
+			In: integrateAccountInput{
+				ClientMutationID: mutationID,
+				EntityID:         entityID,
+				Code:             code,
+			},
+			TestParams: func() *testConnectVendorAccountParams {
+				mra := ramock.New(t)
+				mra.Expect(mock.NewExpectation(mra.Entities, &directory.LookupEntitiesRequest{
+					LookupKeyType: directory.LookupEntitiesRequest_ENTITY_ID,
+					LookupKeyOneof: &directory.LookupEntitiesRequest_EntityID{
+						EntityID: entityID,
+					},
+				}).WithReturns(([]*directory.Entity)(nil), grpc.Errorf(codes.NotFound, "Not Found")))
+				return &testConnectVendorAccountParams{
+					p: graphql.ResolveParams{
+						Info: graphql.ResolveInfo{
+							RootValue: map[string]interface{}{
+								raccess.ParamKey: mra,
+							},
+						},
+					},
+					finishers: []mock.Finisher{mra},
+				}
+			}(),
+			ExpectedErr: nil,
+			Expected: &integrateAccountOutput{
+				ClientMutationID: mutationID,
+				Success:          false,
+				ErrorCode:        integrateAccountErrorCodeEntityNotFound,
+				ErrorMessage:     fmt.Sprintf("Entity %s Not Found", entityID),
+			},
+		},
+		"Error-NotOrg": {
+			In: integrateAccountInput{
+				ClientMutationID: mutationID,
+				EntityID:         entityID,
+				Code:             code,
+			},
+			TestParams: func() *testConnectVendorAccountParams {
+				mra := ramock.New(t)
+				mra.Expect(mock.NewExpectation(mra.Entities, &directory.LookupEntitiesRequest{
+					LookupKeyType: directory.LookupEntitiesRequest_ENTITY_ID,
+					LookupKeyOneof: &directory.LookupEntitiesRequest_EntityID{
+						EntityID: entityID,
+					},
+				}).WithReturns([]*directory.Entity{
+					{
+						Type: directory.EntityType_INTERNAL,
+					},
+				}, nil))
+				return &testConnectVendorAccountParams{
+					p: graphql.ResolveParams{
+						Info: graphql.ResolveInfo{
+							RootValue: map[string]interface{}{
+								raccess.ParamKey: mra,
+							},
+						},
+					},
+					finishers: []mock.Finisher{mra},
+				}
+			}(),
+			ExpectedErr: nil,
+			Expected: &integrateAccountOutput{
+				ClientMutationID: mutationID,
+				Success:          false,
+				ErrorCode:        integrateAccountErrorCodeEntityNotSupported,
+				ErrorMessage:     fmt.Sprintf("Entity %s is not supported for this integration. Expect an Organization", entityID),
+			},
+		},
 		"Success": {
 			In: integrateAccountInput{
 				ClientMutationID: mutationID,
@@ -35,6 +110,16 @@ func TestConnectVendorStripeAccount(t *testing.T) {
 			},
 			TestParams: func() *testConnectVendorAccountParams {
 				mra := ramock.New(t)
+				mra.Expect(mock.NewExpectation(mra.Entities, &directory.LookupEntitiesRequest{
+					LookupKeyType: directory.LookupEntitiesRequest_ENTITY_ID,
+					LookupKeyOneof: &directory.LookupEntitiesRequest_EntityID{
+						EntityID: entityID,
+					},
+				}).WithReturns([]*directory.Entity{
+					{
+						Type: directory.EntityType_ORGANIZATION,
+					},
+				}, nil))
 				mra.Expect(mock.NewExpectation(mra.ConnectVendorAccount, &payments.ConnectVendorAccountRequest{
 					EntityID: entityID,
 					Type:     payments.VENDOR_ACCOUNT_TYPE_STRIPE,
