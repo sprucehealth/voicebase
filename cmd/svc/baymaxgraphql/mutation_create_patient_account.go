c75bc21442470752f1babaac88b62b29bbc14700
Kunal Jham <kunal@sprucehealth.com>
2016-09-22 16:20:36 -0700

    Add intercomToken to authenticate and create account payloads

--- cmd/svc/baymaxgraphql/mutation_create_patient_account.go
+++ cmd/svc/baymaxgraphql/mutation_create_patient_account.go
@@ -207,6 +207,7 @@ var createPatientAccountOutputType = graphql.NewObject(graphql.ObjectConfig{
 		"token":               &graphql.Field{Type: graphql.String},
 		"account":             &graphql.Field{Type: accountInterfaceType},
 		"clientEncryptionKey": &graphql.Field{Type: graphql.String},
+		"intercomToken":       &graphql.Field{Type: intercomTokenType},
 	},
 	IsTypeOf: func(value interface{}, info graphql.ResolveInfo) bool {
 		_, ok := value.(*createPatientAccountOutput)
