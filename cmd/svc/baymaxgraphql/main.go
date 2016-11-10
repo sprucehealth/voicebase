698c69038aa7849ec74be906f226ba485295bc6c
Kunal Jham <kunal@sprucehealth.com>
2016-11-09 18:28:31 -0800

    Allow scheduled messages in non prod environments

--- cmd/svc/baymaxgraphql/main.go
+++ cmd/svc/baymaxgraphql/main.go
@@ -186,6 +186,7 @@ func main() {
 	}
 	patientSyncClient := patientsync.NewPatientSyncClient(conn)
 
+	baymaxgraphqlsettings.ScheduledMessagesConfig.GetBoolean().Default.Value = !environment.IsProd()
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	_, err = settings.RegisterConfigs(
 		ctx,
