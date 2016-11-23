f6bfbd1f511e1b22058bddb35f372f8a63290c95
Samuel Stauffer <samuel@descolada.com>
2016-11-23 10:35:54 -0800

    media: fix race condition (#1210)
    When pulling in media that's not yet in the DB, it's possible for two
    processes to do the work at the same time causing a race. Ignore
    duplicate inserts as it just means someone got there first.

--- libs/dbutil/errors.go
+++ libs/dbutil/errors.go
@@ -2,34 +2,39 @@ package dbutil
 
 import "github.com/go-sql-driver/mysql"
 
-// MySQL warnings
-const (
-	MySQLDuplicateEntry      = "1062"
-	MySQLNoRangeOptimization = "3170" // Memory capacity of N bytes for 'range_optimizer_max_mem_size' exceeded. Range optimization was not done for this query.
+// MySQL error and warning codes
+var (
+	// MySQLDuplicateEntry is returned for inserts that fail a unique key constraint.
+	MySQLDuplicateEntry = MySQLErrorCode{s: "1062", n: 1062}
+	// MySQLDeadlock means a deadlock was found when trying to get lock; try restarting transaction
+	MySQLDeadlock = MySQLErrorCode{s: "1213", n: 1213}
+	// MySQLNoRangeOptimization means the memory capacity of N bytes for 'range_optimizer_max_mem_size' exceeded. Range optimization was not done for this query.
+	MySQLNoRangeOptimization = MySQLErrorCode{s: "3170", n: 3170}
 )
 
-// MySQL error codes
-const (
-	MySQLDeadlock = 1213 // Deadlock found when trying to get lock; try restarting transaction
-)
+// MySQLErrorCode is an error or warning code returned from MySQL
+type MySQLErrorCode struct {
+	s string
+	n uint16
+}
 
 // IsMySQLError returns true if the err represents a MySQL error of the provided code
-func IsMySQLError(err error, code uint16) bool {
+func IsMySQLError(err error, code MySQLErrorCode) bool {
 	e, ok := err.(*mysql.MySQLError)
 	if !ok {
 		return false
 	}
-	return e.Number == code
+	return e.Number == code.n
 }
 
 // IsMySQLWarning returns true if the err represents a MySQL warning of the provided code
-func IsMySQLWarning(err error, code string) bool {
+func IsMySQLWarning(err error, code MySQLErrorCode) bool {
 	warns, ok := err.(mysql.MySQLWarnings)
 	if !ok {
 		return false
 	}
 	for _, w := range warns {
-		if w.Code != code {
+		if w.Code != code.s {
 			return false
 		}
 	}
