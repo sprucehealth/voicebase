150d5647b0da70f19838b9ada6c3abcdb2b55512
Kunal Jham <kunal@sprucehealth.com>
2017-01-17 16:55:25 -0800

    update the character set and collation on the system_title

--- cmd/svc/threading/schema/v35_setup_support_name_change.sql
+++ cmd/svc/threading/schema/v35_setup_support_name_change.sql
@@ -1,3 +1,8 @@
+ALTER TABLE threads MODIFY COLUMN system_title VARCHAR(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+-- set the character set for the mysql client
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_general_ci';
+
 UPDATE threads
 set system_title = 'Spruce Support 🌲'
 WHERE system_title='Spruce Support'
