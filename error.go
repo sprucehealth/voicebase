a83eb63d439f32634582933a6c376b021cdacaa4
Ray Lillywhite <raylillywhite@users.noreply.github.com>
2024-05-11 22:58:37 +0800

    Update error.go

--- error.go
+++ error.go
@@ -6,13 +6,23 @@ type ErrorItem struct {
 	Error string `json:"error"`
 }
 
+type Message struct {
+	Code    int    `json:"code"`
+	Path    string `json:"path"`
+	Message string `json:"message"`
+}
+
 // Error is a structured response indicating a non-2xx HTTP response.
 type Error struct {
-	Errors    ErrorItem `json:"errors"`
+	Warnings  []Message `json:"warnings"`
+	Errors    []ErrorItem `json:"errors"`
 	Reference string    `json:"reference"`
 	Status    int       `json:"status"`
 }
 
 func (e *Error) Error() string {
-	return fmt.Sprintf("voicebase: status=%d %s: %s", e.Status, e.Reference, e.Errors.Error)
+	if len(e.Errors) > 0 {
+		return fmt.Sprintf("voicebase: status=%d %s: %s", e.Status, e.Reference, e.Errors[0].Error)
+	}
+	return ""
 }
